95686a1414020575624b93498101111d5d2bb8c8
Ortega-juuhl <145920496+Ortega-juuhl@users.noreply.github.com>
2024-04-10 23:12:53 +0200

    fixed login

--- login.php
+++ login.php
@@ -3,9 +3,30 @@ session_start();
 
 include 'db_connect.php';
 
-$username = $_POST["username"];
-$password = $_POST["password"];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-$userID = $_SESSION = $userID;
+    $sql = "SELECT * FROM users WHERE username = '$username'";
 
+    if ($result = $conn->query($sql)) {
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $userID = $row['userID'];
+            $_SESSION["userID"] = $userID;
+    
+            $hashed_password = $row['password'];
+            $verify_password = password_verify($password, $hashed_password);
+            if ($verify_password) {
+                echo '<script>alert("Login successful."); window.location.href = "chat_system.html";</script>';
+            } else {
+                echo "Incorrect password.";
+            }
+        } else {
+            echo "Username not found.";
+        }
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
 ?>
