ce72dbe3055f4fe215e5aa6d4e9daf7a9c26fd65
Ortega-juuhl <145920496+Ortega-juuhl@users.noreply.github.com>
2024-04-18 21:21:10 +0200

    fixed remove_friend (chatGPT fixed it)

--- chat_system.php
+++ chat_system.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
+
 include 'db_connect.php';
 
 if (!isset($_SESSION['user_id'])) {
-    // Redirect to login page if user is not logged in
     echo '<script>alert("You need to login before you can use the chat system."); window.location.href = "login.html";</script>';
     exit; // Stop further execution if user is not logged in
 }
 
-// User is logged in, update online status to 'online'
 $user_id = $_SESSION['user_id'];
+
+// User is logged in, update online status to 'online'
 $updateStatusQuery = "UPDATE Users SET online_status = 'online' WHERE user_id = $user_id";
 $conn->query($updateStatusQuery);
 
-// SQL query to fetch names of friends of the current user
-$selectFriendsQuery = "SELECT Users.username 
+// SQL query to fetch names and IDs of friends of the current user
+$selectFriendsQuery = "SELECT Users.user_id, Users.username 
                        FROM Users 
                        INNER JOIN Friends ON Users.user_id = Friends.friend_id 
                        WHERE Friends.user_id = $user_id AND Users.user_id != $user_id";
@@ -71,10 +72,12 @@ $resultSelectFriends = $conn->query($selectFriendsQuery);
         if ($resultSelectFriends->num_rows > 0) {
             echo "Friends: <br> <br>";
             while ($row = $resultSelectFriends->fetch_assoc()) {
-                $username = $row['username']; // Saving the username from $row['username'] to $username variable\
-                echo $username; // Output the username
+                $friend_username = $row['username']; // Save the username from $row['username'] to $friend_username variable
+                $friend_id = $row['user_id']; // Fetch the friend's ID from $row['user_id']
+                echo $friend_username; // Output the username
                 echo "<form action='remove_friend.php' method='post'>";
                 echo "<button type='submit'><i class='fa-solid fa-xmark'></i></button>";
+                echo "<input type='hidden' name='friend_id' value='$friend_id'>"; // Pass the friend's ID as a hidden input field
                 echo "</form>";
             }
         } else {
--- remove_friend.php
+++ remove_friend.php
@@ -1,25 +1,23 @@
 <?php
 session_start();
+
 include 'db_connect.php';
 
 if (!isset($_SESSION['user_id'])) {
     echo '<script>alert("You need to login before you can use the chat system."); window.location.href = "login.html";</script>';
     exit; // Stop further execution if user is not logged in
-} else {
-    $friend_user_id = $_POST['friend_user_id'];
-    $user_id = $_SESSION['user_id'];
-
-    // Delete the friendship in both directions
-    $select_friend = "DELETE FROM Friends WHERE (user_id = $user_id AND friend_id = $friend_user_id) OR (user_id = $friend_user_id AND friend_id = $user_id)";
-    
-    $result = $conn->query($select_friend);
-
-    if ($result) {
-        echo '<script>alert("Successfully removed friend."); window.location.href = "chat_system.php";</script>';
-    } else {
-        echo '<script>alert("Could not remove friend, please try again later."); window.location.href = "chat_system.php";</script>';
-    }
 }
 
-// need to fix the remove friend cause I only get error....... :(((())))
+$user_id = $_SESSION['user_id'];
+$friend_id = $_POST['friend_id'];
+
+$remove_friend = "DELETE FROM Friends WHERE (user_id = $user_id AND friend_id = $friend_id) OR (user_id = $friend_id AND friend_id = $user_id)";
+$result_remove_friend = $conn->query($remove_friend);
+
+if($result_remove_friend) {
+    echo '<script>alert("Removed friend."); window.location.href = "chat_system.php";</script>';
+} else {
+    echo '<script>alert("Error removing friend, please try again later."); window.location.href = "chat_system.php";</script>';
+
+}
 ?>
